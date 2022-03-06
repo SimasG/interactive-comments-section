d7b7a23ef50c8aeeb6d1a559575703d4b5cb9a26
simutis <54304022+SimasG@users.noreply.github.com>
2022-03-06 15:00:58 +0200

    implemented basic json element population

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 displayData();
 
-const commentsContainers = document.querySelectorAll(".comment-container");
+const commentsContainers = document.getElementsByClassName("comment-container");
+const userImgs = document.getElementsByClassName("img-user");
 
 async function getData() {
   const response = await fetch("data.json");
@@ -9,14 +10,8 @@ async function getData() {
 
 async function displayData() {
   const data = await getData();
-  console.log(data);
 
-  commentsContainers.forEach((container) => {
-    container.querySelector(".img-user").src = data["comments"]["user"];
-    // container.querySelectorAll(".img-user").forEach((img) => {
-    //   img.src = data.comments.user.image.png;
-    // });
-    // container.querySelector("[data-element='img']").src =
-    //   data.currentUser.image.png;
-  });
+  for (let i = 0; i < data.comments.length; i++) {
+    userImgs[i].src = data.comments[i].user.image.png;
+  }
 }
