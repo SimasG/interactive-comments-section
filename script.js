0f519e0c57aa2c56959ef0c455eb1c4d02fe74d5
simutis <54304022+SimasG@users.noreply.github.com>
2022-03-06 16:43:36 +0200

    progressed with further html structure and styling

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ displayData();
 
 const commentsContainers = document.getElementsByClassName("comment-container");
 const userImgs = document.getElementsByClassName("img-user");
+const userNames = document.getElementsByClassName("user-name");
+const commentDates = document.getElementsByClassName("comment-date");
+const comments = document.getElementsByClassName("comment");
+const scores = document.getElementsByClassName("score");
 
 async function getData() {
   const response = await fetch("data.json");
@@ -10,8 +14,24 @@ async function getData() {
 
 async function displayData() {
   const data = await getData();
+  console.log(data.comments);
 
   for (let i = 0; i < data.comments.length; i++) {
     userImgs[i].src = data.comments[i].user.image.png;
+  }
+  for (let i = 0; i < data.comments.length; i++) {
+    userNames[i].textContent = data.comments[i].user.username;
+  }
+
+  for (let i = 0; i < data.comments.length; i++) {
+    commentDates[i].textContent = data.comments[i].createdAt;
+  }
+
+  for (let i = 0; i < data.comments.length; i++) {
+    comments[i].textContent = data.comments[i].content;
+  }
+
+  for (let i = 0; i < data.comments.length; i++) {
+    scores[i].textContent = data.comments[i].score;
   }
 }
