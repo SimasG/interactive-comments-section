9c09449b33cf605b0674599d0366fd5bb8e3f48b
simutis <54304022+SimasG@users.noreply.github.com>
2022-03-07 18:27:48 +0200

    fixed new current user comment layout

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const commentDates = document.getElementsByClassName("comment-date");
 const comments = document.getElementsByClassName("comment");
 const scores = document.getElementsByClassName("score");
 const main = document.querySelector("main");
+const userReplies = document.querySelector(".user-replies");
 
 async function getData() {
   const response = await fetch("data.json");
@@ -63,7 +64,7 @@ async function displayData() {
       </div>
     </div>
   </section>`;
-    main.insertAdjacentHTML("beforeend", commentHtml);
+    userReplies.insertAdjacentHTML("beforeend", commentHtml);
   }
 
   //   populating the comments
@@ -130,7 +131,7 @@ async function displayData() {
                 </div>
               </div>
             </section>`;
-        main.insertAdjacentHTML("beforeend", replyHtml);
+        userReplies.insertAdjacentHTML("beforeend", replyHtml);
       });
     }
   }
@@ -151,79 +152,92 @@ function formatDaysAgo(value, locale) {
   const deltaDaysPrecise = Math.round(deltaDays);
   if (deltaDaysPrecise > -7) {
     const formatter = new Intl.RelativeTimeFormat(locale);
+    if (
+      formatter.format(Math.round(deltaDays), "days") === "in 0 days" ||
+      formatter.format(Math.round(deltaDays), "days") === "0 days ago"
+    ) {
+      let finalFormatter = formatter.format(Math.round(deltaDays), "days");
+      finalFormatter = "now";
+      return finalFormatter;
+    }
     return formatter.format(Math.round(deltaDays), "days");
   } else if (deltaDaysPrecise < -7 && deltaDaysPrecise > -31) {
     deltaDays = (date.getTime() - Date.now()) / (1000 * 3600 * 24 * 7);
     const formatter = new Intl.RelativeTimeFormat(locale);
     return formatter.format(Math.round(deltaDays), "weeks");
-  } else if (deltaDaysPrecise < -31) {
+  } else if (deltaDaysPrecise < -31 && deltaDaysPrecise > -365.25) {
     deltaDays = (date.getTime() - Date.now()) / (1000 * 3600 * 24 * 30.42);
     const formatter = new Intl.RelativeTimeFormat(locale);
     return formatter.format(Math.round(deltaDays), "months");
+  } else if (deltaDaysPrecise < -365.25) {
+    deltaDays = (date.getTime() - Date.now()) / (1000 * 3600 * 24 * 30.42 * 12);
+    const formatter = new Intl.RelativeTimeFormat(locale);
+    return formatter.format(Math.round(deltaDays), "years");
   }
 }
 
-// function formatWeeksAgo(value, locale) {
-//     if (formatDaysAgo(value) > )
-// }
-
-console.log(formatDaysAgo("2022-01-09"));
-
-// console.log(formatDaysAgo(new Date(1646656123078)));
-
 function addComment(comment) {
-  const commentHtml2 = `<section class="container comment-container">
-    <div class="comment-info">
-      <img class="img-user" data-element="img" alt="" src=${comment.user} />
-      <p class="user-name">${comment.username}</p>
-      <p class="comment-date">${comment.createdAt}</p>
-    </div>
-    <div>
-      <p class="comment">${comment.content}</p>
-    </div>
-    <div class="vote-reply-container">
-      <div class="vote-btn-container">
-        <svg
-          class="icon-plus"
-          width="11"
-          height="11"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
-            fill="#C5C6EF"
-          />
-        </svg>
-        <p class="score">0</p>
-        <svg
-          class="icon-minus"
-          width="11"
-          height="3"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"
-            fill="#C5C6EF"
-          />
-        </svg>
+  const currentUserCommentHtml = `<section class="comment-reply-container">
+    <div class="left-line"></div>
+    <div class="container comment-reply-subcontainer">
+      <div class="comment-info comment-info-reply">
+        <img
+          class="img-user img-user-reply"
+          data-element="img"
+          src=${comment.user}
+          alt=""
+        />
+        <p class="user-name user-name-reply">${comment.username}</p>
+        <p class="comment-date comment-date-reply">${formatDaysAgo(
+          new Date(comment.createdAt).toISOString()
+        )}</p>
       </div>
-      <div class="btn-reply-container">
-        <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z"
-            fill="#5357B6"
-          />
-        </svg>
-        <button class="btn-reply">Reply</button>
+      <div>
+        <p class="comment comment-reply">${comment.content}</p>
+      </div>
+      <div class="vote-reply-container vote-reply-container-reply">
+        <div class="vote-btn-container vote-btn-container-reply">
+          <svg
+            class="icon-plus"
+            width="11"
+            height="11"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
+              fill="#C5C6EF"
+            />
+          </svg>
+          <p class="score score-reply">0</p>
+          <svg
+            class="icon-minus"
+            width="11"
+            height="3"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"
+              fill="#C5C6EF"
+            />
+          </svg>
+        </div>
+        <div class="btn-reply-container btn-reply-container-reply">
+          <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z"
+              fill="#5357B6"
+            />
+          </svg>
+          <button class="btn-reply btn-reply-reply">Reply</button>
+        </div>
       </div>
     </div>
   </section>`;
-  main.insertAdjacentHTML("beforeend", commentHtml2);
+  userReplies.insertAdjacentHTML("beforeend", currentUserCommentHtml);
 }
 
 async function createComment() {
   const data = await getData();
-  //   const txt = document.querySelector(".current-user-comment");
   const txt = document.querySelector("textarea");
 
   let comment = {
@@ -233,8 +247,6 @@ async function createComment() {
     replyingTo: "maxblagun",
     username: data.currentUser.username,
   };
-
-  //   console.log(comment);
   addComment(comment);
   txt.value = "";
 }
